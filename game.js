c18a02387cae9470e05df8eef588883a0f2efbea
deepwubs <pchucklambs@gmail.com>
2023-06-12 20:34:56 -0500

    pseudo code

---
+++ game.js
@@ -0,0 +1,26 @@
+// variables
+    // num wins
+    //num losses
+    //num ties
+// players choose R, P, S
+    // human choice - get from user input
+    //computer randomly chooses
+
+// compare choices
+    // if humanChoice is S and computerChoice is P OR
+    // if humanChoice is R and computerChoice is S OR
+    // if humanChoice is P and computerChoice is R
+        // alert we win
+        // incrase wins
+    //if humanChoice === computerChoice
+        // alert we tied
+        // increase ties
+    //else
+        // alert we lost
+        // incrase losses
+
+// ask user "Do you want to play again?"
+    // if yes
+        // restart
+    // if no
+        //exit game
