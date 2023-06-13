ba5f1d29c4dd6680bdf800edc0432abe93d8627d
deepwubs <pchucklambs@gmail.com>
2023-06-12 21:17:50 -0500

    scoring

--- game.js
+++ game.js
@@ -4,15 +4,40 @@ var ties = 0
 
 var choices = ["R", "P", "S"]
 
-// human choice - get from user input
+// human choice
 var humanChoice = prompt("Choose R, P, or S")
 console.log(humanChoice)
 
 //computer randomly chooses
 var randomNum = Math.floor(Math.random() * 3)
-console.log(choices[randomNUM])
+var computerChoice = choices[randomNum]
+
+console.log("Human: " + humanChoice)
+console.log("Computer: " + computerChoice)
 
 // compare choices
+if (
+    humanChoice === "S" && computerChoice === "P" || 
+    humanChoice === "R" && computerChoice === "R" ||
+    humanChoice === "P" && computerChoice === "R"
+) {
+    alert("You won!")
+    wins++
+        // incrase wins
+} else if (humanChoice === computerChoice) {
+    alert("You tied!")
+    ties++
+        // increase ties
+} else {
+    alert("You lost!")
+    losses++
+        // incrase losses    
+}
+
+// show score
+alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
+
+
     // if humanChoice is S and computerChoice is P OR
     // if humanChoice is R and computerChoice is S OR
     // if humanChoice is P and computerChoice is R
